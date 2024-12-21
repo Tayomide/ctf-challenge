18049557b4e16664d6226e904427ba195dbf5abc
Tayomide <tibrahim@hawk.iit.edu>
2024-12-21 13:05:25 -0500

    Create website

---
+++ src/components/Doc.md
@@ -0,0 +1,9 @@
+# Welcome to Phantom's Lab!
+
+Hey there! Thanks for visiting my humble little corner of the internet...
+
+This is my first program, and I thought I'd stick to the basics so I don't make
+any mistakes
+
+**Your first task**: Give me a string and
+tell me how long it is.
---
+++ src/components/Terminal.jsx
@@ -0,0 +1,227 @@
+import { useEffect, useRef, useState } from "react"
+import TerminalLog from "./TerminalLog"
+import "../styles/terminal.css"
+// Terminal
+// Input
+
+// Logs
+// Type
+// Input
+// Reply
+
+const Terminal = () => {
+  const [logs, setLogs] = useState([])
+  const [inputType, setInputType] = useState("userString")
+  const [userString, setUserString] = useState(null)
+  const [userStringLength, setUserStringLength] = useState(null)
+  const [input, setInput] = useState("")
+  const [prev, setPrev] = useState("")
+  const inputRef = useRef()
+  const inputTypeRef = useRef()
+  const stringRef = useRef()
+  const stringLengthRef = useRef()
+
+  const focusOnInput = () => {
+    if(inputRef.current){
+      inputRef.current.focus()
+    }
+  }
+
+  const validateInput = (inputType, input) => {
+    switch(inputType){
+      case "userString":
+        if(input.length === 0){
+          return "Looks like you forgot your string, I can't read your mind you know"
+        }
+        return true
+      case "userStringLength":
+      case "stringIdx":
+        if(input.length === 0){
+          return "Looks like you forgot your integer, I can't read your mind you know"
+        }
+        if(isNaN(input) || Number(input) !== parseInt(input)){
+          return "I'll need an integer for this magic trick, stay with me now"
+        }
+        return true
+      default:
+        return true
+    }
+  }
+
+  const getChar = async (string, length, idx) => {
+    const result = {}
+    try{
+      const response = await fetch('/api/check-flag', { 
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "str": string, "idx": idx, "length": length })
+      })
+
+      if(response.ok)result["status"] = "success"
+      else result["status"] = "error"
+
+      const data = await response.json()
+      result["response"] = data?.response
+    }catch{
+      result["status"] = "error"
+      console.log("Catch is working here too")
+    }
+    console.log("Result: ", result)
+    return result
+  }
+
+  const handleKeyDown = async (e) => {
+    if (e.key === 'Enter') {
+      const currentInputType = inputTypeRef.current, currentInput = inputRef.current.value
+      const validation = validateInput(currentInputType, currentInput)
+      setInput("")
+      if(validation !== true){
+        setLogs(prevLogs => {
+          const logCopy = [...prevLogs]
+          logCopy.push({
+            input: currentInput,
+            inputType: currentInputType,
+            display: validation,
+            displayType: "error"
+          })
+          return logCopy
+        })
+        return
+      }
+      switch(currentInputType){
+        case "userString":
+          setUserString(currentInput)
+          setInputType("userStringLength")
+          setLogs(prevLogs => {
+            const logCopy = [...prevLogs]
+            logCopy.push({
+              input: currentInput,
+              inputType: currentInputType
+            })
+            return logCopy
+          })
+          break
+        case "userStringLength":
+          setUserStringLength(parseInt(currentInput))
+          setInputType("stringIdx")
+          setLogs(prevLogs => {
+            const logCopy = [...prevLogs]
+            logCopy.push({
+              input: currentInput,
+              inputType: currentInputType
+            })
+            return logCopy
+          })
+          break
+        case "stringIdx": // API call here
+
+          const result = await getChar(stringRef.current, stringLengthRef.current, parseInt(currentInput) )
+          setLogs(prevLogs => {
+            const logCopy = [...prevLogs]
+            logCopy.push({
+              input: currentInput,
+              inputType: currentInputType,
+              display: result["response"],
+              displayType: result["status"] === "error" ? "error" : undefined
+            })
+            return logCopy
+          })
+          console.log("Result: ",result)
+          break
+        default:
+          break
+
+      }
+      // Do something when Enter is pressed
+    }
+  }
+
+  const handleInput = (e) => {
+    setInput(e.target.value)
+  }
+
+  useEffect(() => {
+    focusOnInput()
+
+    const storedLogs = localStorage.getItem("logs")
+    if(storedLogs){
+      const parsedLogs = JSON.parse(storedLogs)
+
+      let userString = null, userStringLength = null
+      for(const {input, inputType, display, displayType} of parsedLogs){
+        if(display === undefined && displayType === undefined && validateInput(inputType, input) === true){
+          if(inputType === "userString")userString = input
+          else if(inputType === "userStringLength" && userString !== null)userStringLength = parseInt(input)
+        }
+      }
+
+      if(userString === null)setInputType("userString")
+      else if(userStringLength === null)setInputType("userStringLength")
+      else setInputType("stringIdx")
+
+      setUserString(userString)
+      setUserStringLength(userStringLength)
+      setLogs(parsedLogs)
+    }
+    window.addEventListener("click", focusOnInput)
+    window.addEventListener("keydown", handleKeyDown)
+    return () => {
+      window.removeEventListener("click", focusOnInput)
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("logs", JSON.stringify(logs))
+  }, [logs])
+  
+  useEffect(() => {
+    inputTypeRef.current = inputType
+    switch (inputType) {
+      case "userString":
+        setPrev("String: ")
+        break;
+      case "userStringLength":
+        setPrev("Length: ")
+        break;
+      case "stringIdx":
+        setPrev("String Index: ")
+        break
+      default:
+        break;
+    }
+  }, [inputType])
+
+  useEffect(() => {
+    stringRef.current = userString
+  }, [userString])
+
+  useEffect(() => {
+    stringLengthRef.current = userStringLength
+  }, [userStringLength])
+  return (
+    <>
+      <ul id="logs">
+        {logs.map((log, idx) => <TerminalLog {...log} key={idx}/> )}
+      </ul>
+      <label htmlFor="user-input">
+        <span>{prev}</span>
+        <input
+          type="text"
+          id="user-input"
+          name="user-input"
+          ref={inputRef}
+          value={input}
+          onInput={handleInput}
+          onLoad={focusOnInput}
+          autoComplete="off"
+          auto
+        />
+      </label>
+    </>
+  )
+}
+
+export default Terminal
