6863b9150b77fad747d88b81aee8b13035d5e6d9
Tayomide <tibrahim@hawk.iit.edu>
2024-12-24 18:36:38 -0500

    Update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { handler } from './dist/server/entry.mjs'; // Import the Astro server handler
 import http from 'http';
 
-const port = process.env.PORT || 3000; // Render provides PORT environment variable
+const port = 8080; // Render provides PORT environment variable
 
 // Create an HTTP server to handle requests
 const server = http.createServer(handler);
